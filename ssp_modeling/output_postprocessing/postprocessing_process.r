07f962f605bd8f22a4745bb0eb63f743887895e0
fabian fuentes <cffuentes@tec.mx>
2025-07-21 17:34:00 -0600

    new runs 250521 & Tableau

---
+++ ssp_modeling/output_postprocessing/postprocessing_process.r
@@ -0,0 +1,11 @@
+#################################################
+# Post processing process
+#################################################
+
+# load packages
+library(data.table)
+
+
+rm(list=ls())
+
+source('ssp_modeling/output_postprocessing/scr/data_prep_drivers.r')
